0b2e1ce4fbcb32698e32fe89a4c145e5cb5c8548
sujeet kumar <sujeetkr503@gmail.com>
2025-07-01 17:28:32 +0530

    feat: integrate Tailwind CSS, add routing, and create Income page
    - Added Tailwind CSS and related dependencies to the project.
    - Implemented routing using react-router-dom for navigation between Login, SignUp, Home, Income, and Expense pages.
    - Created a Root component to handle user authentication and redirection.
    - Added a new Income page component.
    - Set up global styles with Tailwind CSS and custom font imports.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom'
+import Login from './pages/Auth/Login'
+import SignUp from './pages/Auth/SignUp'
+import Home from './pages/Dashboard/Home'
+import Income from './pages/Dashboard/Income'
+import Expense from './pages/Dashboard/Expense'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='text-3xl'>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/signup" exact element={<SignUp />} />
+          <Route path="/dashboard" exact element={<Home />} />
+          <Route path="/income" exact element={<Income />} />
+          <Route path="/expense" exact element={<Expense />} />
+          {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
-export default App
+export default App;
+
+const Root = () => {
+  // check if user is authenticated or token is present
+  const isAuthenticated = !!localStorage.getItem('token');
+
+  // Redirect to Dashboard if authenticated , otherwise redirect to Login
+  return isAuthenticated ? <Navigate to="/dashboard" replace /> : <Navigate to="/login" replace />;
+};

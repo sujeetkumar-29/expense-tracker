7872f7cdf1ada968fe40e83db485d26dabdbcf66
sujeet kumar <sujeetkr503@gmail.com>
2025-07-03 01:14:03 +0530

    implement dashboard data controller and routes for income and expense tracking

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import connectDB from './config/db.js';
 import authRoutes from './routes/authRoutes.js';
 import incomeRoutes from './routes/incomeRoutes.js';
 import expenseRoutes from './routes/expenseRoutes.js';
+import dashboardRoutes from './routes/dashboardRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -35,6 +36,8 @@ connectDB();
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/income", incomeRoutes);
 app.use("/api/v1/expense", expenseRoutes);
+app.use("/api/v1/dashboard", dashboardRoutes);
+
 
 
 // Serve static files from the 'uploads' directory

50c4ff5b0c5dc54c666fae5dba67ee0ac5f46988
sujeet kumar <sujeetkr503@gmail.com>
2025-07-03 11:57:46 +0530

    add user context provider and integrate with login functionality; implement axios instance with interceptors and define API paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,23 +10,26 @@ import SignUp from './pages/Auth/SignUp'
 import Home from './pages/Dashboard/Home'
 import Income from './pages/Dashboard/Income'
 import Expense from './pages/Dashboard/Expense'
+import UserProvider from './context/userContext'
 
 
 const App = () => {
   return (
-    <div className="">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Root />} />
-          <Route path="/login" exact element={<Login />} />
-          <Route path="/signup" exact element={<SignUp />} />
-          <Route path="/dashboard" exact element={<Home />} />
-          <Route path="/income" exact element={<Income />} />
-          <Route path="/expense" exact element={<Expense />} />
-          {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
-        </Routes>
-      </Router>
-    </div>
+    <UserProvider>
+      <div className="">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Root />} />
+            <Route path="/login" exact element={<Login />} />
+            <Route path="/signup" exact element={<SignUp />} />
+            <Route path="/dashboard" exact element={<Home />} />
+            <Route path="/income" exact element={<Income />} />
+            <Route path="/expense" exact element={<Expense />} />
+            {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
+          </Routes>
+        </Router>
+      </div>
+    </UserProvider>
   )
 }
 
--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,62 +1,86 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import AuthLayout from '../../components/layouts/AuthLayout'
 import { Link, useNavigate } from 'react-router-dom'
 import Input from '../../components/Inputs/Input'
 import { validateEmail } from '../../utils/helper'
+import { API_PATHS } from '../../utils/apiPaths'
+import axiosInstance from '../../utils/axiosInstance'
+import { UserContext } from '../../context/userContext'
 
 const Login = () => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [error, setError] = useState(null)
 
+  const { updateUser } = useContext(UserContext)
+
   const navigate = useNavigate()
 
   // Handle login form submission
   const handleLogin = async (e) => {
     e.preventDefault();
-    if(!validateEmail(email)){
+    if (!validateEmail(email)) {
       setError("Please enter a valid email address.")
       return;
     }
-    if(!password){
+    if (!password) {
       setError("Please enter the password")
       return;
     }
     setError("");
     // Login api call
-
+    try {
+      const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
+        email,
+        password,
+      });
+      const { token, user } = response.data;
+      if (token) {
+        localStorage.setItem("token", token);
+        // localStorage.setItem("user", JSON.stringify(response.data.user));
+        updateUser(user);
+        navigate("/dashboard");
+      }
+    } catch (error) {
+      if (error.response && error.response.data.message) {
+        setError(error.response.data.message);
+      }
+      else {
+        setError("Something went wrong. Please try again.");
+      }
+    }
   }
   return (
     <AuthLayout>
       <div className="lg-w[70%] h-3/4 md:h-full flex flex-col justify-center">
-      <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
-      <p className="text-xs text-slate-500 mb-6 mt-[5px]">
-        Please enter your credentials to access your account.
-      </p>
-        <form  onSubmit={handleLogin}>
-          <Input 
-           value={email}
+        <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
+        <p className="text-xs text-slate-500 mb-6 mt-[5px]">
+          Please enter your credentials to access your account.
+        </p>
+        <form onSubmit={handleLogin}>
+          <Input
+            value={email}
             onChange={(e) => setEmail(e.target.value)}
             label="Email Address"
             placeholder="Enter your email"
             type="email"
-           />
-          <Input 
-           value={password}
+          />
+          <Input
+            value={password}
             onChange={(e) => setPassword(e.target.value)}
             label="Password"
             placeholder="Enter your password"
             type="password"
-            
-           />
+
+          />
           {error && <p className="text-red-500 text-xs mt-2">{error}</p>}
           <button
             type="submit"
-            className=" btn-primary "   
+            className=" btn-primary "
           >
             Login
           </button>
-        <p className="text-[13px] text-slate-500 mt-4">
+          <p className="text-[13px] text-slate-500 mt-4">
             Don't have an account?{" "}
             <Link className="font-medium text-primary underline" to="/signup">
               Sign Up

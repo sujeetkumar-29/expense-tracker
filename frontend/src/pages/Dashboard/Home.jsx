706877e3dd1d607f76789268a23d7f592569d415
sujeet kumar <sujeetkr503@gmail.com>
2025-07-04 10:19:10 +0530

    add CustomPieChart, CustomTooltip, and CustomLegend components; integrate FinanceOverview into Home dashboard for enhanced financial visualization

--- frontend/src/pages/Dashboard/Home.jsx
+++ frontend/src/pages/Dashboard/Home.jsx
@@ -6,10 +6,11 @@ import axiosInstance from '../../utils/axiosInstance'
 import { API_PATHS } from '../../utils/apiPaths'
 import InfoCard from '../../components/Cards/InfoCard'
 
-import { LuHandCoins, LuWalletMinimal} from "react-icons/lu";
-import { IoMdCard } from "react-icons/io" ;
+import { LuHandCoins, LuWalletMinimal } from "react-icons/lu";
+import { IoMdCard } from "react-icons/io";
 import { addThousandsSeparator } from '../../utils/helper'
 import RecentTransactions from '../../components/Dashboard/RecentTransactions'
+import FinanceOverview from '../../components/Dashboard/FinanceOverview'
 
 const Home = () => {
   useUserAuth()
@@ -68,7 +69,12 @@ const Home = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6 ">
           <RecentTransactions
             transactions={dashboardData?.recentTransactions}
-            onSeeMore={()=>navigate("/expense")}
+            onSeeMore={() => navigate("/expense")}
+          />
+          <FinanceOverview
+            totalBalance={dashboardData?.totalBalance || 0}
+            totalIncome={dashboardData?.totalIncome || 0}
+            totalExpenses={dashboardData?.totalExpenses || 0}
             />
         </div>
       </div>

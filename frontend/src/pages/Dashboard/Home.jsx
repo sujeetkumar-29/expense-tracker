c88d1eadb509cac79a2841488729f23e73d53ebe
sujeet kumar <sujeetkr503@gmail.com>
2025-07-04 18:05:57 +0530

    add RecentIncome component and integrate it into Home dashboard for displaying recent income transactions

--- frontend/src/pages/Dashboard/Home.jsx
+++ frontend/src/pages/Dashboard/Home.jsx
@@ -14,6 +14,7 @@ import FinanceOverview from '../../components/Dashboard/FinanceOverview'
 import ExpenseTransactions from '../../components/Dashboard/ExpenseTransactions'
 import Last30DaysExpenses from '../../components/Dashboard/Last30DaysExpenses'
 import RecentIncomeWithChart from '../../components/Dashboard/RecentIncomeWithChart'
+import RecentIncome from '../../components/Dashboard/RecentIncome'
 
 const Home = () => {
   useUserAuth()
@@ -49,7 +50,7 @@ const Home = () => {
   return (
     <DashboardLayout activeMenu="Dashboard">
       <div className="my-5 mx-auto">
-        {/* <div className="grid grid-cols-1 md:grid-cols-3 gap-6 ">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 ">
           <InfoCard
             icon={<IoMdCard />}
             label="Total Balance "
@@ -68,7 +69,7 @@ const Home = () => {
             value={addThousandsSeparator(dashboardData?.totalExpense || 0)}
             color="bg-red-500"
           />
-        </div> */}
+        </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6 ">
           <RecentTransactions
             transactions={dashboardData?.recentTransactions}
@@ -90,6 +91,10 @@ const Home = () => {
             <RecentIncomeWithChart
             data={dashboardData?.last60DaysIncome?.transactions?.slice(0,4) || []}
             totalIncome={dashboardData?.totalIncome || 0 }
+            />
+            <RecentIncome
+            transactions={dashboardData?.last60DaysIncome?.transactions||[]}
+            onSeeMore={()=>navigate("/income")}
             />
         </div>
       </div>

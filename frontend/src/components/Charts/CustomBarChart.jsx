a285a91141a8454f1e7a6dd9bb2fd469920d5183
sujeet kumar <sujeetkr503@gmail.com>
2025-07-08 00:46:30 +0530

    feat: update CustomBarChart and DarkModeToggle for improved dark mode support; enhance tooltip display and button styling

--- frontend/src/components/Charts/CustomBarChart.jsx
+++ frontend/src/components/Charts/CustomBarChart.jsx
@@ -14,15 +14,16 @@ import {
 const CustomBarChart = ({ data }) => {
 
     // Function to get alternative color
-    const getBarColor = (index) => (index % 2 === 0 ? "#875cf5" : "#cfbefb");
+    const getBarColor = (index) => (index % 2 === 0 ? "#6f3ff5" : "#e0d7ff");
 
     const CustomTooltip = ({ active, payload }) => {
 
         if (active && payload && payload.length) {
             return (
-                <div className="bg-white shadow-md rounded-lg p-2 border border-gray-300 ">
-                    <p className="text-xs font-semibold text-purple-800 mb-1">{payload[0].payload.category}</p>
-                    <p className="text-sm text-gray-600">Amount:<span className="text-sm font-medium text-gray-900">₹{payload[0].payload.amount}</span></p>
+                <div className="bg-white dark:bg-gray-900 shadow-md rounded-lg p-2 border border-gray-300 ">
+                    <p className="text-xs font-semibold text-purple-800 dark:text-purple-900 mb-1">{payload[0].payload.source}</p>
+                    <p className="text-xs font-semibold text-purple-800 dark:text-purple-900 mb-1">{payload[0].payload.category}</p>
+                    <p className="text-sm text-gray-600 dark:text-gray-400">Amount:<span className="text-sm font-medium text-gray-900 dark:text-gray-100">₹{payload[0].payload.amount}</span></p>
 
                 </div>
             )
@@ -31,7 +32,7 @@ const CustomBarChart = ({ data }) => {
     }
 
     return (
-        <div className="bg-white mt-6">
+        <div className="bg-white dark:bg-gray-900 mt-6">
             <ResponsiveContainer width="100%" height={300}>
                 <BarChart data={data}>
                     <CartesianGrid stroke="none" />
--- frontend/src/components/DarkModeToggle.jsx
+++ frontend/src/components/DarkModeToggle.jsx
@@ -7,7 +7,7 @@ import { FaSun } from "react-icons/fa";
 const DarkModeToggler = () => {
     const {darkMode,setDarkMode}=useDarkMode()
     return (
-        <button onClick={()=>setDarkMode(!darkMode)} className="dark:text-white">
+        <button onClick={()=>setDarkMode(!darkMode)} className="dark:text-white cursor-pointer">
             {darkMode ? <FaSun /> : <FaMoon className="text-black" />}
         </button>
     )
